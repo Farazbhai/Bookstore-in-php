ad082f9213f90184219c0e6ffae3e8a6e0eeea4d
Farazbhai <farazbhai5154@gmail.com>
2023-10-02 00:29:09 -0700

    I have done my header and footer

--- book-store/Layouts/footer.php
+++ book-store/Layouts/footer.php
@@ -1,4 +1,4 @@
-<div class="bgm container-fluid mt-5">
+<div class="bgm container-fluid mt-5 bg-dark">
         <div class="row">
             <div class="col-sm col-md-3">
                 <span class="mt-5 fw-bold text-info">CONNECT</span>
@@ -6,7 +6,6 @@
                 <p class="text-light">2005 Stokes Isle Apt. 896,</p>
                 <p class="mt-3 text-light">info@yourdomain.com</p>
                 <p class="mt-3 text-warning">(+68) 120034509</p>
-
             </div>
             <div class="col-sm col-md-3">
                 <span class="mt-5 fw-bold text-info">EXTRA LINKS</span>
@@ -14,7 +13,6 @@
                 <p class="text-light">Business Services</p>
                 <p class="mt-3 text-light">Education Services</p>
                 <p class="mt-3 text-light">Gift Cards</p>
-
             </div>
             <div class="col-sm col-md-3">
                 <span class="mt-5 fw-bold text-info">YOUR ACCOUNT</span>
@@ -23,12 +21,11 @@
                 <p class="mt-3 text-light">Gift Collections</p>
                 <p class="mt-3 text-light">Purchase Cards</p>
                 <span class="mt3 text-light">FAQs</span>
-
             </div>
             <div class="col-sm col-md-3">
                 <span class="mt-5 fw-bold text-info">BEST SELLERS</span>
                 <div class="d-flex">
-                    <img class="w-25 mt-2" src="Assats/image/9.jpg" alt="">
+                    <img class="w-25 mt-2" src="../assets/images/50.jpg" alt="Image not found">
                     <div class="d-flex flex-column m-3">
                         <p class="text-light">Henry is the good dog</p>
                         <p class="text-light">$49.00 – $60.00</p>
@@ -40,11 +37,10 @@
     <div class="container-fluid bg-black">
         <div class="row">
             <div class="col-sm col-lg-6">
-                <img src="Assats/image/11.jpg" alt="">
-                <img src="Assats/image/10.jpg" alt="">
-                <img src="Assats/image/12.jpg" alt="">
-                <img src="Assats/image/13.jpg" alt="">
-            </div>
+                <img src="../assets/images/11.jpg" alt="image not found">
+                <img src="../assets/images/12.jpg" alt="image not found">
+                <img src="../assets/images/13.jpg" alt="image not found">
+                </div>
             <div class="col-sm col-lg-6 d-flex justify-content-end">
                 <span class="text-light">© 2018 Auteur. All Right Reserved.</span>
             </div>
---
+++ book-store/Layouts/navbar.php
@@ -0,0 +1,49 @@
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-lg-1">
+            <i class="fa fa-bars mx-3" aria-hidden="true"></i>
+        </div>
+        <div class="col-lg-1">
+            <h6 class="mx-3">HOMES</h6>
+        </div>
+        <div class="col-lg-1">
+            <a class=" text-dark text-decoration-none " href="#">
+                <h6 class="mx-3"><a class="text-decoration-none text-dark" href="#"> FEATURES</a></h6>
+            </a>
+        </div>
+        <div class="col-lg-1">
+            <a class=" text-dark text-decoration-none " href="#">
+                <h6 class="mx-3"><a class="text-decoration-none text-dark" href="#"> PAGES</a></h6>
+            </a>
+        </div>
+        <div class="col-lg-2">
+            <img class="mx-4" src="../assets/images/logo-dark.png" width="150px" alt="image not found">
+        </div>
+        <div class="col-lg-1">
+            <a class=" text-dark text-decoration-none " href="#">
+                <h6 class="mx-3">SHOP</h6>
+            </a>
+        </div>
+        <div class="col-lg-1">
+            <a class=" text-dark text-decoration-none " href="#">
+                <h6 class="mx-3">EVENT</h6>
+            </a>
+        </div>
+        <div class="col-lg-1">
+            <a class=" text-dark text-decoration-none " href="# ">
+                <h6 class="mx-3">BLOG</h6>
+            </a>
+        </div>
+        <div class="col-lg-1">
+            <i class="fa fa-search mx-3 " aria-hidden="true"></i>
+        </div>
+        <div class="col-lg-1">
+            <a class="text-dark d-flex" href="Add to card.html"> <i class="fa fa-shopping-cart mx-3" aria-hidden="true">
+                    <h4 id="countCartItems"></h4>
+                </i></a>
+        </div>
+        <div class="col-lg-1">
+            <i class="fa fa-user-o mx-3" aria-hidden="true"></i>
+        </div>
+    </div>
+</div>
