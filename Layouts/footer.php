f739c532a0606c38bc69dd384c9b8a67ac037992
Farazbhai <farazbhai5154@gmail.com>
2023-10-01 22:34:40 -0700

    I have created my nevbar and footer

---
+++ Layouts/footer.php
@@ -0,0 +1,55 @@
+<div class="bgm container-fluid mt-5">
+        <div class="row">
+            <div class="col-sm col-md-3">
+                <span class="mt-5 fw-bold text-info">CONNECT</span>
+                <p class="mt-3 text-light">2005 Stokes Isle Apt. 896,</p>
+                <p class="text-light">2005 Stokes Isle Apt. 896,</p>
+                <p class="mt-3 text-light">info@yourdomain.com</p>
+                <p class="mt-3 text-warning">(+68) 120034509</p>
+
+            </div>
+            <div class="col-sm col-md-3">
+                <span class="mt-5 fw-bold text-info">EXTRA LINKS</span>
+                <p class="mt-3 text-light">Affiliate Program</p>
+                <p class="text-light">Business Services</p>
+                <p class="mt-3 text-light">Education Services</p>
+                <p class="mt-3 text-light">Gift Cards</p>
+
+            </div>
+            <div class="col-sm col-md-3">
+                <span class="mt-5 fw-bold text-info">YOUR ACCOUNT</span>
+                <p class="mt-3 text-light">Shop</p>
+                <p class="text-light">My Orders</p>
+                <p class="mt-3 text-light">Gift Collections</p>
+                <p class="mt-3 text-light">Purchase Cards</p>
+                <span class="mt3 text-light">FAQs</span>
+
+            </div>
+            <div class="col-sm col-md-3">
+                <span class="mt-5 fw-bold text-info">BEST SELLERS</span>
+                <div class="d-flex">
+                    <img class="w-25 mt-2" src="Assats/image/9.jpg" alt="">
+                    <div class="d-flex flex-column m-3">
+                        <p class="text-light">Henry is the good dog</p>
+                        <p class="text-light">$49.00 – $60.00</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid bg-black">
+        <div class="row">
+            <div class="col-sm col-lg-6">
+                <img src="Assats/image/11.jpg" alt="">
+                <img src="Assats/image/10.jpg" alt="">
+                <img src="Assats/image/12.jpg" alt="">
+                <img src="Assats/image/13.jpg" alt="">
+            </div>
+            <div class="col-sm col-lg-6 d-flex justify-content-end">
+                <span class="text-light">© 2018 Auteur. All Right Reserved.</span>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
