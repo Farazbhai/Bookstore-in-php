0b4ea0ae9636231c076eb964cdf434d27b73da69
Farazbhai <farazbhai5154@gmail.com>
2023-10-04 21:05:23 -0700

    i have done

--- book-store/Layouts/navbar.php
+++ book-store/Layouts/navbar.php
@@ -7,40 +7,37 @@
                 <h6 class="mx-3">HOMES</h6>
             </div>
             <div class="col-lg-1">
-                <a class=" text-dark text-decoration-none " href="features.html">
-                    <h6 class="mx-3"><a class="text-decoration-none text-dark" href="Feature.html"> FEATURES</a></h6>
+                <a class=" text-dark text-decoration-none " href="#">
+                    <h6 class="mx-3"><a class="text-decoration-none text-dark"> FEATURES</a></h6>
                 </a>
             </div>
             <div class="col-lg-1">
-                <a class=" text-dark text-decoration-none " href="pages.html">
-                    <h6 class="mx-3"><a class="text-decoration-none text-dark" href="Pages.html"> PAGES</a></h6>
+                <a class=" text-dark text-decoration-none " href="#">
+                    <h6 class="mx-3"><a class="text-decoration-none text-dark" href="#"> PAGES</a></h6>
                 </a>
             </div>
             <div class="col-lg-2">
-                <img class="mx-4" src="../assets/images/logo-dark.png" width="150px" alt="">
+                <img class="mx-4" src="../assets/images/logo-dark.png" width="150px" alt="#">
             </div>
             <div class="col-lg-1">
-                <a class=" text-dark text-decoration-none " href="shop.html">
+                <a class=" text-dark text-decoration-none " href="books.php">
                     <h6 class="mx-3">SHOP</h6>
                 </a>
             </div>
             <div class="col-lg-1">
-                <a class=" text-dark text-decoration-none " href="">
+                <a class=" text-dark text-decoration-none " href="about_Us.php">
                     <h6 class="mx-3">About</h6>
                 </a>
             </div>
             <div class="col-lg-1">
-                <a class=" text-dark text-decoration-none " href="blog.html">
-                    <h6 class="mx-3">Contact</h6>
+                    <a href="Contact_Us.php"><h6 class="mx-3">Contact</h6></a>
                 </a>
             </div>
-
             <div class="col-lg-1">
                 <i class="fa fa-search mx-3 " aria-hidden="true"></i>
-
             </div>
             <div class="col-lg-1">
-                <a class="text-dark d-flex" href="Add to card.html"> <i class="fa fa-shopping-cart mx-3"
+                <a class="text-dark d-flex" href=""> <i class="fa fa-shopping-cart mx-3"
                         aria-hidden="true">
                         <h4 id="countCartItems"></h4>
                     </i></a>
---
+++ book-store/books.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include "layouts/header.php" ?>
+
+<body>
+    <?php include "layouts/navbar.php" ?>
+    <div class="min-min">
+        <div class="abot">
+            <h1 class="display-1">SHOP</h1>
+            <p>Home/About Us 01</p>
+        </div>
+    </div>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-8">
+                <p>Showing 13–21 of 21 results</p>
+            </div>
+            <div class="col-md-1">
+                <i class="fa fa-bars" aria-hidden="true"></i><span>Filters</span>
+            </div>
+            <div class="col-md-2 min text-center">
+                <p class="mt-1">Default Sorting</p>
+            </div>
+            <div class="col-md-1 d-flex mt-2">
+                <i class="fa fa-th mx-3" aria-hidden="true"></i>
+                <i class="fa fa-align-justify" aria-hidden="true"></i>
+            </div>
+        </div>
+        <hr class="mt-3">
+    </div>
+    <div class="container mt-5">
+        <div class="row w-100 text-center">
+            <div class="col-sm col-md-3">
+                <img class="w-100" src="assets/images/1.jpg" alt="image not found">
+                <p class="text-center mt-3"><del>$24.00 </del> $22.00</p>
+                <span class="spn fw-bold">Bulle & Pelle</span>
+                <p class="pera mt-3">BySAVANNA WALKER</p>
+                QNTY:<input class="w-25 rounded border-1 " type="number"><br>
+                <button class="mt-2 rounded border-1 border-black bg-black text-light p-2">Add to
+                    card</button><br>
+            </div>
+            <div class="col-sm col-md-3">
+                <img class="w-100" src="assets/images/2.jpg" alt="image not found">
+                <p class="text-center mt-3"><del>$19.00</del> $16.00</p>
+                <span class="spn fw-bold">Peter and the Wolf</span>
+                <p class="pera mt-3">ByJOHN WALKER</p>
+                QNTY:<input class="w-25 rounded border-1 " type="number"><br>
+                <button class="mt-2 rounded border-1 border-black bg-black text-light p-2">Add to
+                    card</button><br>
+            </div>
+            <div class="col-sm col-md-3">
+                <img class="w-100" src="assets/images/3.jpg" alt="image not found">
+                <p class="text-center mt-3">$24.00</p>
+                <span class="spn fw-bold">When the Doves</span>
+                <p class="pera mt-3">ByHOF NURGIN</p>
+                QNTY:<input class="w-25 rounded border-1 " type="number"><br>
+                <button class="mt-2 rounded border-1 border-black bg-black text-light p-2">Add to
+                    card</button><br>
+            </div>
+            <div class="col-sm col-md-3">
+                <img class="w-100" src="assets/images/4.jpg" alt="image not found">
+                <p class="text-center mt-3">$19.00</p>
+                <span class="spn fw-bold">The Assault</span>
+                <p class="pera mt-3">ByMESHO BUVAHR,</p>
+                QNTY:<input class="w-25 rounded border-1 " type="number"><br>
+                <button class="mt-2 rounded border-1 border-black bg-black text-light p-2">Add tocard</button><br>
+            </div>
+        </div>
+    </div>
+    <div class="container mt-5">
+        <div class="row w-100 text-center">
+            <div class="col-sm col-md-3">
+                <img class="w-100" src="assets/images/5.jpg" alt="image not found">
+                <p class="text-center mt-3"><del>$24.00 </del> $22.00</p>
+                <span class="spn fw-bold">Bulle & Pelle</span>
+                <p class="pera mt-3">BySAVANNA WALKER</p>
+                QNTY:<input class="w-25 rounded border-1 " type="number"><br>
+                <button class="mt-2 rounded border-1 border-black bg-black text-light p-2">Add to
+                    card</button><br>
+            </div>
+            <div class="col-sm col-md-3">
+                <img class="w-100" src="assets/images/6.jpg" alt="image not found">
+                <p class="text-center mt-3"><del>$19.00</del> $16.00</p>
+                <span class="spn fw-bold">Peter and the Wolf</span>
+                <p class="pera mt-3">ByJOHN WALKER</p>
+                QNTY:<input class="w-25 rounded border-1 " type="number"><br>
+                <button class="mt-2 rounded border-1 border-black bg-black text-light p-2">Add to
+                    card</button><br>
+            </div>
+            <div class="col-sm col-md-3">
+                <img class="w-100" src="assets/images/7.jpg" alt="image not found">
+                <p class="text-center mt-3">$24.00</p>
+                <span class="spn fw-bold">When the Doves</span>
+                <p class="pera mt-3">ByHOF NURGIN</p>
+                QNTY:<input class="w-25 rounded border-1 " type="number"><br>
+                <button class="mt-2 rounded border-1 border-black bg-black text-light p-2">Add to
+                    card</button><br>
+            </div>
+            <div class="col-sm col-md-3">
+                <img class="w-100" src="assets/images/8.jpg" alt="image not found">
+                <p class="text-center mt-3">$19.00</p>
+                <span class="spn fw-bold">The Assault</span>
+                <p class="pera mt-3">ByMESHO BUVAHR,</p>
+                QNTY:<input class="w-25 rounded border-1 " type="number"><br>
+                <button class="mt-2 rounded border-1 border-black bg-black text-light p-2">Add to
+                    card</button><br>
+            </div>
+        </div>
+    </div>
+    <div class="container mt-5">
+        <div class="row w-100 text-center">
+            <div class="col-sm col-md-3">
+                <img class="w-100" src="assets/images/k4.jpg" alt="image not found">
+                <p class="text-center mt-3"><del>$24.00 </del> $22.00</p>
+                <span class="spn fw-bold">Bulle & Pelle</span>
+                <p class="pera mt-3">BySAVANNA WALKER</p>
+                QNTY:<input class="w-25 rounded border-1 " type="number"><br>
+                <button class="mt-2 rounded border-1 border-black bg-black text-light p-2">Add to
+                    card</button><br>
+            </div>
+            <div class="col-sm col-md-3">
+                <img class="w-100" src="assets/images/k3.jpg" alt="image not found">
+                <p class="text-center mt-3"><del>$19.00</del> $16.00</p>
+                <span class="spn fw-bold">Peter and the Wolf</span>
+                <p class="pera mt-3">ByJOHN WALKER</p>
+                QNTY:<input class="w-25 rounded border-1 " type="number"><br>
+                <button class="mt-2 rounded border-1 border-black bg-black text-light p-2">Add to
+                    card</button><br>
+            </div>
+            <div class="col-sm col-md-3">
+                <img class="w-100" src="assets/images/k2.jpg" alt="image not found">
+                <p class="text-center mt-3">$24.00</p>
+                <span class="spn fw-bold">When the Doves</span>
+                <p class="pera mt-3">ByHOF NURGIN</p>
+                QNTY:<input class="w-25 rounded border-1 " type="number"><br>
+                <button class="mt-2 rounded border-1 border-black bg-black text-light p-2">Add to
+                    card</button><br>
+            </div>
+            <div class="col-sm col-md-3">
+                <img class="w-100" src="assets/images/k1.jpg" alt="image not found">
+                <p class="text-center mt-3">$19.00</p>
+                <span class="spn fw-bold">The Assault</span>
+                <p class="pera mt-3">ByMESHO BUVAHR,</p>
+                QNTY:<input class="w-25 rounded border-1 " type="number"><br>
+                <button class="mt-2 rounded border-1 border-black bg-black text-light p-2">Add to
+                    card</button><br>
+            </div>
+        </div>
+    </div>
+    <div class="container mt-5">
+        <div class="row w-100 text-center">
+            <div class="col-sm col-md-3">
+                <img class="w-100" src="assets/images/kkkkkkkkk.jpg" alt="image not found">
+                <p class="text-center mt-3"><del>$24.00 </del> $22.00</p>
+                <span class="spn fw-bold">Bulle & Pelle</span>
+                <p class="pera mt-3">BySAVANNA WALKER</p>
+                QNTY:<input class="w-25 rounded border-1 " type="number"><br>
+                <button class="mt-2 rounded border-1 border-black bg-black text-light p-2">Add to
+                    card</button><br>
+            </div>
+            <div class="col-sm col-md-3">
+                <img class="w-100" src="assets/images/mmmmmmm.jpg" alt="image not found">
+                <p class="text-center mt-3"><del>$19.00</del> $16.00</p>
+                <span class="spn fw-bold">Peter and the Wolf</span>
+                <p class="pera mt-3">ByJOHN WALKER</p>
+                QNTY:<input class="w-25 rounded border-1 " type="number"><br>
+                <button class="mt-2 rounded border-1 border-black bg-black text-light p-2">Add to
+                    card</button><br>
+            </div>
+            <div class="col-sm col-md-3">
+                <img class="w-100" src="assets/images/nnnnnn.jpg" alt="image not found">
+                <p class="text-center mt-3">$24.00</p>
+                <span class="spn fw-bold">When the Doves</span>
+                <p class="pera mt-3">ByHOF NURGIN</p>
+                QNTY:<input class="w-25 rounded border-1 " type="number"><br>
+                <button class="mt-2 rounded border-1 border-black bg-black text-light p-2">Add to
+                    card</button><br>
+            </div>
+            <div class="col-sm col-md-3">
+                <img class="w-100" src="assets/images/yyyyyyyy.jpg" alt="Image not found">
+                <p class="text-center mt-3">$19.00</p>
+                <span class="spn fw-bold">The Assault</span>
+                <p class="pera mt-3">By MESHO BUVAHR,</p>
+                QNTY:<input class="w-25 rounded border-1 " type="number"><br>
+                <button class="mt-2 rounded border-1 border-black bg-black text-light p-2">Add to
+                    card</button><br>
+            </div>
+        </div>
+    </div>
+    <?php include "layouts/footer.php" ?>
+
+</body>
+
+</html>
